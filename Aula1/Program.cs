ca14efc70d4fbf7c5a3316e27ca3b63e6cca3114
Arthurtech8 <iarthurti@gmail.com>
2026-07-15 21:27:06 -0300

    Soma,divisao,subtracao,multiplicacao

--- Aula1/Program.cs
+++ Aula1/Program.cs
@@ -1,30 +1,33 @@
 ﻿using System;
-class Program
+namespace Aula1
 {
-    static void Main()
+
+    class Program
     {
-        Console.Write("Digite seu nome: ");
-        string nome = Console.ReadLine();
-
-        Console.Write("Digite sua idade: ");
-        int idade = int.Parse(Console.ReadLine());
-        int a = 24;
-
-        if (idade >= 24)
+        static void Maink()
         {
-            Console.WriteLine($"Olá, {nome}! Você é maior de idade. Tem {idade}");
-        }
-        else if (idade <= 12)
+            Console.Write("Digite seu nome: ");
+            string nome = Console.ReadLine();
 
-            Console.WriteLine($"Olá, {nome}! Você é menor de idade. Tem {idade}");
-    }
-        else
-        {
-            Console.WriteLine($"Olá, {nome}! Você é adolescente. Tem {idade}");
+            Console.Write("Digite sua idade: ");
+            int idade = int.Parse(Console.ReadLine());
+            int a = 24;
+
+            if (idade >= 24)
+            {
+                Console.WriteLine($"Olá, {nome}! Você é maior de idade. Tem {idade}");
+            }
+            else if (idade <= 12)
+
+                Console.WriteLine($"Olá, {nome}! Você é menor de idade. Tem {idade}");
+
+            else
+            {
+                Console.WriteLine($"Olá, {nome}! Você é adolescente. Tem {idade}");
+            }
+            Console.WriteLine($"A vida está passando, {nome}!");
+            Console.WriteLine($"Já foram {idade} anos de vida.");
         }
-Console.WriteLine($"A vida está passando, {nome}!");
-Console.WriteLine($"Já foram {idade} anos de vida.");
     }
+
 }
-
-teste commit
--- Aula2/Class1.cs
+++ Aula2/Class1.cs
@@ -1,30 +1,40 @@
 ﻿using System;
-class Program
+namespace Aula2
+
 {
-    static void Main()
+    class Program
     {
-        Console.Write("Digite seu nome: ");
-        string nome = Console.ReadLine();
-
-        Console.Write("Digite sua idade: ");
-        int idade = int.Parse(Console.ReadLine());
-        int a = 24;
-
-        if (idade >= 24)
+        static void Main()
         {
-            Console.WriteLine($"Olá, {nome}! Você é maior de idade. Tem {idade}");
-        }
-        else if (idade <= 12)
+            Console.Write("Digite o primeiro número: ");
+            int numero1 = int.Parse(Console.ReadLine());
 
-            Console.WriteLine($"Olá, {nome}! Você é menor de idade. Tem {idade}");
-    }
-        else
-        {
-            Console.WriteLine($"Olá, {nome}! Você é adolescente. Tem {idade}");
+            Console.Write("Digite o segundo número: ");
+            int numero2 = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite por quanto quer subtrair: ");
+            int numero3 = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite por quanto quer multiplicar: ");
+            int numero4 = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite por quanto quer Dividir: ");
+            int numero5 = int.Parse(Console.ReadLine());
+
+            int soma = numero1 + numero2;
+            Console.Write($" A soma é: {soma}");
+
+            int subtracao = soma - numero3;
+            Console.Write($" A subtração é: {subtracao}");
+
+            int multiplicacao = subtracao * numero4;
+            Console.Write($" A multiplicação é: {multiplicacao}");
+
+            int divisao = multiplicacao / numero5;
+            Console.Write($" A divisão é: {divisao}");
+
+
         }
-Console.WriteLine($"A vida está passando, {nome}!");
-Console.WriteLine($"Já foram {idade} anos de vida.");
     }
+
 }
-
-teste commit
