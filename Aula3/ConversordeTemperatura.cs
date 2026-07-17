d6f16b8e0071c2fe33ae50271401e287472282c1
Arthurtech8 <iarthurti@gmail.com>
2026-07-16 21:59:08 -0300

    Temperatura

---
+++ Aula3/ConversordeTemperatura.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class ConversordeTemperatura
+{
+    static void Main()
+    {
+        Console.Write("Digite a temperatura em Celsius: ");
+        double celsius = Convert.ToDouble(Console.ReadLine());
+        double fahrenheit = (celsius * 9 / 5) + 32;
+        Console.WriteLine($"A temperatura em Fahrenheit é: {fahrenheit} °F");
+        //Console.ReadKey();
+
+        double celsiusconv = (fahrenheit - 32) * 5 / 9;
+        Console.WriteLine($"A temperatura em Celsius é: {celsiusconv} °");
+        Console.ReadKey();
+    }
+
+} 
--- Aula3/baseretangulo.cs
+++ Aula3/baseretangulo.cs
@@ -1,19 +1,17 @@
-﻿using.System;
+﻿using System;
 
-namespace.ConsoleApp1.Aula3
+class Baseretangulo
 {
-    internal class baseretangulo
+    static void Main5()
     {
-        static void Main()
-        {
-            Console.Write("Digite a base do retângulo: ");
-            double BaseRetangulo = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Digite a base do retângulo: ");
+        double BaseRetangulo = Convert.ToDouble(Console.ReadLine());
 
-            Console.Write("Digite a altura do retângulo: ");
-            double AlturaRetangulo = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Digite a altura do retângulo: ");
+        double AlturaRetangulo = Convert.ToDouble(Console.ReadLine());
 
-            int multiplicacao = BaseRetangulo * AlturaRetangulo;
-            Console.Write($" A área do retângulo é: {multiplicacao}");
-        }
+        double multiplicacao = BaseRetangulo * AlturaRetangulo;
+        Console.WriteLine($" A área do retângulo é: {multiplicacao}");
+        Console.ReadKey();
     }
 }
